fdf58ff55bc627fb5d7885473e130b83b1c0bb8d
Matheus Araldi <araldi_matheus@hotmail.com>
2023-05-15 17:59:30 -0300

    added new folder

--- run.py
+++ run.py
@@ -2,10 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/<numero>", methods=['GET','POST'])
+@app.route("/int:<numero>", methods=['GET','POST'])
 def hello(numero):
     return 'Hello {}'.format(numero)
 
 if __name__=="__main__":
-    
+
     app.run(debug=True)
