d444acfad1000a9ff04a99b6b66e3c558fb3472f
Matheus Araldi <araldi_matheus@hotmail.com>
2023-05-19 17:14:14 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,17 +3,20 @@ from flask import Flask, jsonify, request, json
 app = Flask(__name__)
 
 desenvolvedores = [
-    {'nome':'Matheus',
+    {'id':'0',
+     'nome':'Matheus',
      'habilidades':['Python, SQL'],
      },
-     {'nome':'Araldi',
+     {'id':'1',
+      'nome':'Araldi',
       'habilidades':['Java, Python']}
 ]
+
 
 @app.route('/dev/<int:id>',methods = ['GET','PUT','DELETE'])
 def desenvolvedor(id):
     """
-    nome do desenvolver
+    devolve um desenvolvedor pelo ID, também deleta ou altera um desenvolvedor.
     """
     if request.method == 'GET':
         try:
@@ -34,6 +37,21 @@ def desenvolvedor(id):
     elif request.method == 'DELETE':
         desenvolvedores.pop(id)
         return jsonify({'status':'Sucesso','mensagem':'Registro Excluído'})
+@app.route('dev/',methods = ['POST', 'GET'])
+def lista_desenvolvedores():
+    """
+    Lista todos os desenvolvedores e inclui um novo desenvolvedor.
+    """
+    if request.method == 'POST':
+        dados = json.loads(request.data)
+        posicao = len(desenvolvedores)
+        dados[id] = posicao
+        desenvolvedores.append(dados)
+        return jsonify({'status':'sucesso', 'mensagem':'Registro inserido!'})
+    elif request.method == 'GET':
+        return jsonify(desenvolvedores)
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
